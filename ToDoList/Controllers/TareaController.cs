5de5e2464c97ee0a6b15de1b4c3fee3fe119c596
Pablo Rodriguez Navarro <prodriguez99@hotmail.com>
2023-10-02 13:31:47 -0600

    Se agrega vistazo inicial del homepage del tablero

---
+++ ToDoList/Controllers/TareaController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class TareaController : Controller
+    {
+        Tarea[] tareas = new Tarea[]
+        {
+            new Tarea
+            {
+                Titulo = "Bases II",
+                Descripcion = "Quiz de bases",
+                FechaInicial = DateTime.Now,
+                FechaFinal = DateTime.Now.AddDays(5),
+                Estado = 1,
+                Categoria = 2,
+                Prioridad = 3,
+                Dificultad = 3,
+            },
+            new Tarea
+            {
+                Titulo = "Diseno Proyecto",
+                Descripcion = "Entrega de interfaces sin funcionalidad",
+                FechaInicial = DateTime.Now,
+                FechaFinal = DateTime.Now.AddDays(3),
+                Estado = 2,
+                Categoria = 1,
+                Prioridad = 4,
+                Dificultad = 4,
+            },
+            new Tarea
+            {
+                Titulo = "Asistencia",
+                Descripcion = "Completar las horas semanales",
+                FechaInicial = DateTime.Now,
+                FechaFinal = DateTime.Now.AddDays(2),
+                Estado = 1,
+                Categoria = 4,
+                Prioridad = 4,
+                Dificultad = 1,
+            },
+            new Tarea
+            {
+                Titulo = "Asistencia",
+                Descripcion = "Completar las horas semanales",
+                FechaInicial = DateTime.Now,
+                FechaFinal = DateTime.Now.AddDays(2),
+                Estado = 3,
+                Categoria = 4,
+                Prioridad = 4,
+                Dificultad = 1,
+            },
+        };
+
+        public IActionResult Index()
+        {
+            ViewBag.Tareas = tareas;
+            return View();
+        }
+    }
+}
---
+++ ToDoList/Models/Tarea.cs
@@ -0,0 +1,16 @@
+﻿namespace ToDoList.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaInicial { get; set; }
+        public DateTime FechaFinal { get; set; }
+        public int Dificultad { get; set; }
+        public int Prioridad { get; set; }
+        public int UsuarioCreador { get; set; }
+        public int Categoria { get; set; }
+        public int Estado { get; set; }
+    }
+}
