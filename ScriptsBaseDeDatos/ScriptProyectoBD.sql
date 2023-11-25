27654bcbfb83488c553928132d373d29213e6673
Sabrina Brenes Hernández <saindell.brenes@ucr.ac.cr>
2023-11-24 20:57:42 -0600

    Script para ingresar datos de juegos de prueba

--- ScriptsBaseDeDatos/ScriptProyectoBD.sql
+++ ScriptsBaseDeDatos/ScriptProyectoBD.sql
@@ -53,6 +53,11 @@ DROP TABLE Usuario;
 INSERT INTO Usuario (Nombre, PrimerApellido, SegundoApellido, Email, EsUsuarioDeJuego)
 VALUES ('Admin', '', '', 'AdminDefault@gmail.com', 'false');
 
+
+INSERT INTO Usuario (Nombre, PrimerApellido, SegundoApellido, Email, EsUsuarioDeJuego)
+VALUES ('The Legend of Zelda', '', '', 'Zelda@gmail.com', 'true'),
+		('Dark Souls I', '', '', 'Solaire@gmail.com', 'true');
+
 INSERT INTO Estado (NombreEstado, IdentificadorUsuarioCreador)
 Values ('Pendientes', 'F547EF79-DB4A-4803-B74C-0BF7EC66D4391'), ('En Curso', 'F547EF79-DB4A-4803-B74C-0BF7EC66D439'), ('Completadas', 'F547EF79-DB4A-4803-B74C-0BF7EC66D439') 
 
@@ -67,3 +72,17 @@ VALUES ('Sabrina', 'Brenes', 'Hernandez', 'sabry.brenes@outlook.es', 'false'),
 INSERT INTO Tarea (Titulo, Descripcion, FechaInicial, FechaFinal, Dificultad, Prioridad, IdentificadorUsuarioCreador, IdentificadorCategoria, IdentificadorEstado)
 VALUES ('Proyecto Bases Avanzadas', 'Investigacion de base de datos Scylla', '2023-10-05', '2023-10-12', '5', '3', 'D156E9CE-405A-41E3-BBD7-A21FA5E5FE53', '3', '0'),
 		('Laboratorio Bases Avanzadas', 'Lab de ETL', '2023-10-06', '2023-10-09', '3', '3', 'D156E9CE-405A-41E3-BBD7-A21FA5E5FE53', '3', '1')
+
+
+
+INSERT INTO Tarea (Titulo, Descripcion, FechaInicial, FechaFinal, Dificultad, Prioridad, IdentificadorUsuarioCreador, IdentificadorCategoria, IdentificadorEstado)
+VALUES ('Derrota al jefe final', 'Derrota al Señor Oscuro: Ganondorf', '2023-12-05', '', '5', '3', '42425333-BD31-4A6A-B2A3-85D6980199B1', '16', '0'),
+		('Derrota al jefe del Patíbulo del Desierto', 'Derrota al Fosíl de las Sombras: Stallord', '2023-12-05', '2023-12-15', '3', '3', '42425333-BD31-4A6A-B2A3-85D6980199B1', '16', '2'),
+		('Derrota al jefe de Celestia', 'Derrota al Dragón de las Sombras: Argorok', '2023-12-05', '2023-12-15', '4', '3', '42425333-BD31-4A6A-B2A3-85D6980199B1', '16', '2'),
+		('Derrota al jefe del Palacio del Crepúsculo', 'Derrota al Rey Usurpador: Zant', '2023-12-05', '', '5', '3', '42425333-BD31-4A6A-B2A3-85D6980199B1', '16', '0'),
+		('Derrota al jefe de Anor Londo', 'Derrota a Ornstein el Asesino de dragones y Smough el Verdugo', '2023-12-02', '2023-12-27', '5', '3', 'D67C161B-76BC-483A-B130-B9A5C7EE05CA', '16', '2'),
+		('Derrota al jefe de la Tumba de los gigantes', 'Derrota a Nito el Rey del Cementerio', '2023-12-02', '', '5', '3', 'D67C161B-76BC-483A-B130-B9A5C7EE05CA', '16', '0'),
+		('Derrota al jefe del Bosque Real', 'Derrota al Caballero Artorias', '2023-12-02', '', '5', '1', 'D67C161B-76BC-483A-B130-B9A5C7EE05CA', '16', '0');
+
+Delete from Tarea where Tarea.IdentificadorUsuarioCreador = '42425333-BD31-4A6A-B2A3-85D6980199B1'
+Delete from Usuario where Usuario.IdentificadorUsuario = '42425333-BD31-4A6A-B2A3-85D6980199B1'
